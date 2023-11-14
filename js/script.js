8b5d631beddc4274c97d55b6f8106933025292e1
Motiduki <yonetu.s.1216@gmail.com>
2023-11-15 08:18:31 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+$(function(){
+  $('.owl-carousel').owlCarousel({
+    items: 3,
+    loop: true,
+    autoplay: true,
+    fluidSpeed: 0,
+    smartSpeed: 2000,
+    autoplayTimeout: 2000,
+  });
+});
+
